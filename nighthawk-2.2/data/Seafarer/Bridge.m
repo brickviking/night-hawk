328211164abd7a0f3a20e3a7a270bb1380133112
cvs2git <>
2004-05-03 08:53:06 +0000

    This commit was manufactured by cvs2svn to create tag 'nighthawk_2_3'.

--- nighthawk-2.2/data/Seafarer/Bridge.m
+++
@@ -1,4 +0,0 @@
-door: 112 48 v
-transport: 48 144
-console: 48 80
-noise: 1 200 40
--- nighthawk-2.2/data/Tobruk/Hull.m
+++
@@ -1,9 +0,0 @@
-door: 144 80 v
-door: 112 144 h
-door: 912 144 v
-door: 1680 144 v
-transport: 880 144
-transport: 1712 144
-power_bay: 48 272
-console: 112 176
-noise: 0 240 55
