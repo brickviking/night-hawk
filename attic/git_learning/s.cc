9dbc72ffabcb5bdba59b40f0d2d5f24b3114751b
jsno8192 <jsno8192@gmail.com>
2020-10-26 13:30:32 +1030

    new file:   s.cc

---
+++ attic/git_learning/s.cc
@@ -0,0 +1,47 @@
+/*
+	This is a test.
+*/
+#include <stdio.h>
+
+class A
+{
+public:
+	A(void);
+	void print_hello(void);
+};
+
+A::A(void)
+{
+	printf("Init A\n");
+}
+
+void A::print_hello(void)
+{
+	printf("Hello A\n");
+}
+
+class B : A
+{
+public:
+	B(void);
+	void print_hello(void);
+};
+
+B::B(void)
+{
+	printf("Init B\n");
+}
+
+void B::print_hello(void)
+{
+	printf("Hello B\n");
+}
+
+int main(void)
+{
+	B x;
+
+	x.print_hello();
+
+	return 0;
+}
